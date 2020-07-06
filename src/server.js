f8e05183a9e64b0fe84c1d19c26d13bb33867523
Amit032 <amitnit2k18@gmail.com>
2020-07-06 16:35:27 +0530

    changes server file for heroku deployment

--- src/server.js
+++ src/server.js
@@ -1,24 +1,20 @@
-const express = require("express");
-
-const port = process.env.PORT || 8080;
+var express = require('express');
 var app = express();
-
-// List of all the files that should be served as-is
 let protectd = ['transformed.js', 'main.css', 'favicon.ico']
+app.use(express.static(__dirname + '/'));
+app.listen(process.env.PORT || 8080);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get("*", (req, res) => {
 
-app.get("*", (req, res) => {
-
-  let path = req.params['0'].substring(1)
-
-  if (protectd.includes(path)) {
-    // Return the actual file
-    res.sendFile(`${__dirname}/build/${path}`);
-  } else {
-    // Otherwise, redirect to /build/index.html
-    res.sendFile(`${__dirname}/build/index.html`);
+        let path = req.params['0'].substring(1)
+      
+        if (protectd.includes(path)) {
+          // Return the actual file
+          res.sendFile(`${__dirname}/build/${path}`);
+        } else {
+          // Otherwise, redirect to /build/index.html
+          res.sendFile(`${__dirname}/build/index.html`);
+        }
+      });
   }
-});
-
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
-});
