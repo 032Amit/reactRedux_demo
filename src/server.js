000c26ea2c2b04fa31ef54283fa42e77ab70ea62
Amit032 <amitnit2k18@gmail.com>
2020-07-06 17:16:55 +0530

    moved pocfile

--- src/server.js
+++ src/server.js
@@ -1,20 +1,14 @@
 var express = require('express');
 var app = express();
-let protectd = ['transformed.js', 'main.css', 'favicon.ico']
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
     app.get("*", (req, res) => {
-
         let path = req.params['0'].substring(1)
-      
-        if (protectd.includes(path)) {
-          // Return the actual file
-          res.sendFile(`${__dirname}/build/${path}`);
-        } else {
-          // Otherwise, redirect to /build/index.html
-          res.sendFile(`${__dirname}/build/index.html`);
-        }
-      });
+        let url = path.join(__dirname, '../client/build', 'index.html');
+        if (!url.startsWith('/app/')) // since we're on local windows
+          url = url.substring(1);
+        res.sendFile(url);
+    });
   }
