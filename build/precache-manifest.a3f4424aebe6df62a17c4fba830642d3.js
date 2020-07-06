2db08c8bbd1df19e0cb6a9f46cf1697859dd680b
Amit032 <amitnit2k18@gmail.com>
2020-07-06 16:12:52 +0530

    Configure heroku deployment2

---
+++ build/precache-manifest.a3f4424aebe6df62a17c4fba830642d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6060c2714cc71d0c09f2",
+    "url": "/static/css/main.322a35e8.chunk.css"
+  },
+  {
+    "revision": "6060c2714cc71d0c09f2",
+    "url": "/static/js/main.6060c271.chunk.js"
+  },
+  {
+    "revision": "91d86993e6f9a84724a2",
+    "url": "/static/js/1.91d86993.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b8b8e4109fd729981627d6fe8b4d8d77",
+    "url": "/static/media/byjusLogo.b8b8e410.PNG"
+  },
+  {
+    "revision": "1677649fdf5aa3849a8dff1ec36a67dc",
+    "url": "/index.html"
+  }
+];
